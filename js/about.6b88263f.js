b035fdcb1a70f2eac642e8c57e1ee8cd4cac3472
noemifarre <144995847+noemifarre@users.noreply.github.com>
2023-12-20 13:26:30 +0100

    Add files via upload

---
+++ js/about.6b88263f.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkesto_es_otra_prueba"]=self["webpackChunkesto_es_otra_prueba"]||[]).push([[443],{6493:function(t,e,s){s.r(e),s.d(e,{default:function(){return _}});var u=function(){var t=this;t._self._c;return t._m(0)},n=[function(){var t=this,e=t._self._c;return e("div",{staticClass:"about"},[e("h1",[t._v("This is an about page")])])}],a=s(1001),r={},l=(0,a.Z)(r,u,n,!1,null,null,null),_=l.exports}}]);
+//# sourceMappingURL=about.6b88263f.js.map
